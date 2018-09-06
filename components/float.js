499169cbe6a997552ec9e45f35f75413d56b1940
attharmirza <atthar.mirza@gmail.com>
2018-09-06 01:32:03 -0400

    Failed Raycasting
    I can't get raycasting to work with ThreeJS within AFrame. I suspect it
    has something to do with the render function in ThreeJS.
    
    Anyway, next I'm going to give it a shot with AFrame's built in
    raycaster.

--- components/float.js
+++ components/float.js
@@ -2,27 +2,47 @@
 // var THREE = require('../../lib/three');
 
 AFRAME.registerComponent('floating', {
-  
+
   schema: {
-    width: {type: 'number', default: 1},
-    depth: {type: 'number', default: 1}
+    width: {
+      type: 'number',
+      default: 1
+    },
+    depth: {
+      type: 'number',
+      default: 1
+    }
   },
-  
-  init: function () {
+
+  update: function() {
     this.raycaster = new THREE.Raycaster();
-  },
-  
-  tick: function () {
+
     var data = this.data;
     var el = this.el;
     var rotation = el.components.rotation.data;
     var position = el.components.position.data;
-    var scene = this.el.sceneEl.object3D;
     var raycaster = this.raycaster;
-    
-    raycaster.set(position, rotation);
-    
-    console.log(raycaster.intersectObjects(scene.children));
+    var scene = this.el.sceneEl.object3D;
+
+    var rayOrigin = new THREE.Vector3(position.x, position.y, position.z);
+    var rayDirection = new THREE.Vector3(0, 0, 0);
+
+    raycaster.set(rayOrigin, rayDirection);
+
+    var arrow = new THREE.ArrowHelper( raycaster.ray.direction, raycaster.ray.origin, 2, '#000000');
+    scene.add( arrow );
+
+    // console.log(raycaster.intersectObjects( scene.children ));
+  },
+
+  tick: function() {
+    var raycaster = this.raycaster;
+    var scene = this.el.sceneEl.object3D;
+    var box = document.querySelector('a-box').object3D.children;
+
+    // console.log(box)
+    // console.log(raycaster);
+    console.log(raycaster.intersectObjects( box ));
   }
-  
+
 });
