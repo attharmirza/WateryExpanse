d7ec3870db1d42fd959a8cfced84ce422ae13229
Glitch (a-watery-expanse) <none>
2018-09-13 17:26:37 +0000

    ☝️🚝 Updated with Glitch

--- components/float.js
+++ components/float.js
@@ -2,27 +2,136 @@
 // var THREE = require('../../lib/three');
 
 AFRAME.registerComponent('floating', {
-  
+
   schema: {
-    width: {type: 'number', default: 1},
-    depth: {type: 'number', default: 1}
+    width: {
+      type: 'number',
+      default: 1
+    },
+    depth: {
+      type: 'number',
+      default: 1
+    }
   },
-  
-  init: function () {
-    this.raycaster = new THREE.Raycaster();
+
+  update: function() {
+    var scene = this.el.sceneEl;
+    var depth = this.data.depth;
+    var width = this.data.width;
+    var dampening = this.data.dampening;
+    var position = this.el.components.position.data;
+
+    var rayCreate = function (element, position) {
+      element.setAttribute('id', 'ray-' + position);
+      // element.setAttribute('raycaster', 'showLine: true;');
+      element.setAttribute('rotation', {
+        x: -90,
+        y: 90,
+        z: 0
+      });
+
+      if (position == 'back') {
+        element.setAttribute('position', {
+          x: 0,
+          y: 0,
+          z: 0 + depth
+        });
+      } else if (position == 'front') {
+        element.setAttribute('position', {
+          x: 0,
+          y: 0,
+          z: 0 - depth
+        });
+      } else if (position == 'left') {
+        element.setAttribute('position', {
+          x: 0 - width,
+          y: 0,
+          z: 0
+        });
+      } else if (position == 'right') {
+        element.setAttribute('position', {
+          x: 0 + width,
+          y: 0,
+          z: 0
+        });
+      } else {
+        console.log('please use back, right, left, or front for position');
+      };
+    }
+
+    var rayBack = document.createElement('a-entity');
+    var rayFront = document.createElement('a-entity');
+    var rayLeft = document.createElement('a-entity');
+    var rayRight = document.createElement('a-entity');
+
+    rayCreate(rayBack, 'back');
+    this.el.appendChild(rayBack);
+    this.rayBack = rayBack;
+
+    rayCreate(rayFront, 'front');
+    this.el.appendChild(rayFront);
+    this.rayFront = rayFront;
+
+    rayCreate(rayLeft, 'left');
+    this.el.appendChild(rayLeft);
+    this.rayLeft = rayLeft;
+
+    rayCreate(rayRight, 'right');
+    this.el.appendChild(rayRight);
+    this.rayRight = rayRight;
+    
+    var pointBack;
+    var pointFront;
+    var pointLeft;
+    var pointRight;
+    
+    var oceanTilt = function () {
+      var angleZ;
+      var angleX;
+      
+      angleX = -(Math.atan((pointBack.y + pointFront.y)/(depth * 2))) * (180/Math.PI);
+      angleZ = (Math.atan((pointLeft.y + pointRight.y)/(width * 2))) * (180/Math.PI);
+      
+
+      document.querySelector('#pointAverage').setAttribute('rotation', {x: angleX * dampening, y: 0, z: angleZ * dampening});
+    };
+
+    document.querySelector('#ocean').addEventListener('raycaster-intersected', function (event) {
+      
+      if (event.detail.el == rayBack) {
+        pointBack = event.detail.intersection.point;
+        document.querySelector('#pointBack').setAttribute('position', pointBack);
+      } else if (event.detail.el == rayFront) {
+        pointFront = event.detail.intersection.point;
+        document.querySelector('#pointFront').setAttribute('position', pointFront);
+      } else if (event.detail.el == rayLeft) {
+        pointLeft = event.detail.intersection.point;
+        document.querySelector('#pointLeft').setAttribute('position', pointLeft);
+      } if (event.detail.el == rayRight) {
+        pointRight = event.detail.intersection.point;
+        document.querySelector('#pointRight').setAttribute('position', pointRight); 
+      }
+      
+      var pointAverage = (pointFront.y + pointBack.y + pointLeft.y + pointRight.y)/4;
+      
+      document.querySelector('#pointAverage').setAttribute('position', {y: pointAverage - 10}); 
+      
+      oceanTilt();
+      
+    });
   },
-  
-  tick: function () {
-    var data = this.data;
-    var el = this.el;
-    var rotation = el.components.rotation.data;
-    var position = el.components.position.data;
-    var scene = this.el.sceneEl.object3D;
-    var raycaster = this.raycaster;
+
+  tick: function() {
+    var castRay = function (element) {
+      element.removeAttribute('raycaster');
+      element.setAttribute('raycaster', 'showLine: false;');
+    }
+
+    castRay(this.rayBack);
+    castRay(this.rayFront);
+    castRay(this.rayLeft);
+    castRay(this.rayRight);
     
-    raycaster.set(position, rotation);
-    
-    console.log(raycaster.intersectObjects(scene.children));
   }
-  
+
 });
