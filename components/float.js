5c6943f00fa65fa8342d346c2017d7343f0efe37
Glitch (a-watery-expanse) <none>
2018-11-04 00:31:57 +0000

    🎰🤒 Updated with Glitch

--- components/float.js
+++ components/float.js
@@ -1,6 +1,10 @@
 AFRAME.registerComponent('floating', {
 
   schema: {
+    target: {
+      type: 'selector',
+      default: '',
+    },
     width: {
       type: 'number',
       default: 1
@@ -8,7 +12,7 @@ AFRAME.registerComponent('floating', {
     depth: {
       type: 'number',
       default: 1
-    }, 
+    },
     dampening: {
       type: 'array',
       default: [1, 1, 1]
@@ -26,85 +30,100 @@ AFRAME.registerComponent('floating', {
     var dampening = this.data.dampening;
     var surface = this.data.surface;
     var position = this.el.components.position.data;
+    var target = this.data.target;
+    var targetPosition = {};
     
+    // console.log(targetPosition);
+    
+    if (this.data.target) {
+      target = this.data.target;
+    } else {
+      target = el.children[0]
+    };
+
     this.el.setAttribute('position', {x: position.x, y: position.y + 10, z: position.z });
 
-    var rayCreate = function (element, position) {
-      element.setAttribute('id', 'ray-' + position);
+    var rayCreate = function (element, orientation, initialPosition) {
+      element.setAttribute('id', 'ray-' + orientation);
       element.setAttribute('rotation', {
         x: -90,
         y: 90,
         z: 0
       });
 
-      if (position == 'back') {
+      if (orientation == 'back') {
         element.setAttribute('position', {
-          x: 0,
+          x: initialPosition.x,
           y: 0,
-          z: 0 + depth
+          z: initialPosition.z + depth
         });
-      } else if (position == 'front') {
+      } else if (orientation == 'front') {
         element.setAttribute('position', {
-          x: 0,
+          x: initialPosition.x,
           y: 0,
-          z: 0 - depth
+          z: initialPosition.z - depth
         });
-      } else if (position == 'left') {
+      } else if (orientation == 'left') {
         element.setAttribute('position', {
-          x: 0 - width,
+          x: initialPosition.x - width,
           y: 0,
-          z: 0
+          z: initialPosition.z
         });
-      } else if (position == 'right') {
+      } else if (orientation == 'right') {
         element.setAttribute('position', {
-          x: 0 + width,
+          x: initialPosition.x + width,
           y: 0,
-          z: 0
+          z: initialPosition.z
         });
       } else {
         console.log('please use back, right, left, or front for position');
       };
-    }
+    };
 
     var rayBack = document.createElement('a-entity');
     var rayFront = document.createElement('a-entity');
     var rayLeft = document.createElement('a-entity');
     var rayRight = document.createElement('a-entity');
-
-    rayCreate(rayBack, 'back');
-    this.el.appendChild(rayBack);
-    this.rayBack = rayBack;
-
-    rayCreate(rayFront, 'front');
-    this.el.appendChild(rayFront);
-    this.rayFront = rayFront;
-
-    rayCreate(rayLeft, 'left');
-    this.el.appendChild(rayLeft);
-    this.rayLeft = rayLeft;
-
-    rayCreate(rayRight, 'right');
-    this.el.appendChild(rayRight);
-    this.rayRight = rayRight;
     
+    // target.addEventListener('model-loaded', function () {
+      targetPosition = target.getAttribute('position');
+      
+      rayCreate(rayBack, 'back', targetPosition);
+      el.appendChild(rayBack);
+      
+      rayCreate(rayFront, 'front', targetPosition);
+      el.appendChild(rayFront);
+      
+      rayCreate(rayLeft, 'left', targetPosition);
+      el.appendChild(rayLeft);
+      
+      rayCreate(rayRight, 'right', targetPosition);
+      el.appendChild(rayRight);
+    // });
+    
+    this.rayBack = rayBack;
+    this.rayFront = rayFront;
+    this.rayLeft = rayLeft;
+    this.rayRight = rayRight;
+
     var pointBack;
     var pointFront;
     var pointLeft;
     var pointRight;
-    
+
     var oceanTilt = function () {
       var angleZ;
       var angleX;
-      
+
       angleX = -(Math.atan((pointBack.y + pointFront.y)/(depth * 2))) * (180/Math.PI);
       angleZ = (Math.atan((pointLeft.y + pointRight.y)/(width * 2))) * (180/Math.PI);
-      
 
-      el.children[0].setAttribute('rotation', {x: angleX * dampening[0], y: 0, z: angleZ * dampening[1]});
+
+      target.setAttribute('rotation', {x: angleX * dampening[0], y: 0, z: angleZ * dampening[1]});
     };
 
     document.querySelector(surface).addEventListener('raycaster-intersected', function (event) {
-      
+
       if (event.detail.el == rayBack) {
         pointBack = event.detail.intersection.point;
       } else if (event.detail.el == rayFront) {
@@ -114,36 +133,41 @@ AFRAME.registerComponent('floating', {
       } if (event.detail.el == rayRight) {
         pointRight = event.detail.intersection.point;
       }
-      
+
       var pointAverage = (pointFront.y + pointBack.y + pointLeft.y + pointRight.y)/4;
-      
-      el.children[0].setAttribute('position', {y: (pointAverage * dampening[2]) - 10}); 
-      
+
+      // console.log(targetPosition);
+      target.setAttribute('position', {x: targetPosition.x, y: (pointAverage * dampening[2]) - 10, z: targetPosition.z});
+
       oceanTilt();
-      
+
     });
   },
 
   tick: function() {
-    var castRay = function (element) {
+    var rayCast = function (element) {
       element.removeAttribute('raycaster');
       element.setAttribute('raycaster', 'showLine: false;');
     }
 
-    castRay(this.rayBack);
-    castRay(this.rayFront);
-    castRay(this.rayLeft);
-    castRay(this.rayRight);
-    
+    rayCast(this.rayBack);
+    rayCast(this.rayFront);
+    rayCast(this.rayLeft);
+    rayCast(this.rayRight);
+
   },
-  
+
   remove: {}
 
 });
 
 AFRAME.registerComponent('floating-small', {
 
-  schema: { 
+  schema: {
+    target: {
+      type: 'selector',
+      default: '',
+    },
     surface: {
       type: 'string'
     }
@@ -154,51 +178,65 @@ AFRAME.registerComponent('floating-small', {
     var scene = this.el.sceneEl;
     var surface = this.data.surface;
     var position = this.el.components.position.data;
+    var target;
+    var targetPosition = {};
     
+    if (this.data.target) {
+      target = this.data.target;
+    } else {
+      target = el.children[0]
+    };
+
     this.el.setAttribute('position', {x: position.x, y: position.y + 10, z: position.z });
 
-    var rayCreate = function (element, position) {
-      element.setAttribute('id', 'ray-' + position);
+    var rayCreate = function (element, orientation, initialPosition) {
+      element.setAttribute('id', 'ray-' + orientation);
       element.setAttribute('rotation', {
         x: -90,
         y: 90,
         z: 0
-      }); 
+      });
       element.setAttribute('position', {
-        x: 0,
+        x: initialPosition.x,
         y: 0,
-        z: 0
+        z: initialPosition.z
       });
     }
 
     var rayCenter = document.createElement('a-entity');
     
-    rayCreate(rayCenter, 'center');
-    this.el.appendChild(rayCenter);
+    // target.addEventListener('loaded', function () {
+      targetPosition = target.getAttribute('position');
+      
+      rayCreate(rayCenter, 'center', targetPosition);
+      el.appendChild(rayCenter);
+    // });
+
+
     this.rayCenter = rayCenter;
-    
+
     var pointCenter;
 
     document.querySelector(surface).addEventListener('raycaster-intersected', function (event) {
-      
+
       if (event.detail.el == rayCenter) {
         pointCenter = event.detail.intersection.point;
-      }
-      
-      el.children[0].setAttribute('position', {x: pointCenter.x - position.x, y: pointCenter.y - position.y - 10, z: pointCenter.z - position.z}); 
-      
+      };
+
+      target.setAttribute('position', {x: targetPosition.x, y: (pointCenter.y - position.y) - 10, z: targetPosition.z});
+
     });
   },
 
   tick: function() {
-    var castRay = function (element) {
+    var rayCast = function (element) {
       element.removeAttribute('raycaster');
       element.setAttribute('raycaster', 'showLine: false;');
     }
 
-    castRay(this.rayCenter);
+    rayCast(this.rayCenter);
   },
-  
+
   remove: {},
 
 });
--- components/meshMaterialReset.js
+++ components/meshMaterialReset.js
@@ -8,15 +8,26 @@ AFRAME.registerComponent('mesh-material-reset', {
     var group;
     var objects;
     
-    var material = new THREE.MeshStandardMaterial();
+    // var material = new THREE.MeshStandardMaterial();
+    // var materialAttribute = element.components.material;
+    var material = element.components.material.material;
     
     console.log(material);
     
     this.el.addEventListener('model-loaded', function () {
-      group = element.object3D.children[0];
+      var groupSize = element.object3D.children.length - 1;
+      console.log(groupSize);
+      
+      group = element.object3D.children[groupSize];
       objects = group.children;      
           
       for (i = 0; i < objects.length; i++) {
+        if (document.querySelector('#' + objects[i].name + '-texture') != null) {
+          console.log(objects[i].name + "'s texture exists!");
+        } else {
+          console.log(objects[i].name + "'s texture does not exist, bro!");
+        }
+        
         objects[i].material = material;
       }
     });
